229c4ba8595768774a4e1765f62ed45526eb774a
Micaela Maraschin <mica.maraschin@gmail.com>
2024-06-06 22:25:18 -0300

    Practicando js

---
+++ Primera práctica de js/Ejemplo01.js
@@ -0,0 +1,30 @@
+// let contador = 0;
+// while (contador < 10) {
+// contador = contador + 1;
+// console.log(contador);
+// }
+
+// for (let i = 100; i >= 0; i--) {
+//     console.log(i);
+// }
+
+
+// const edad = 18
+// if (edad >= 18){
+//     alert ('Eres mayor de edad')
+// } else{ alert ('Todavía eres menor de edad')}
+
+
+switch (valor) {
+    case 1: document.write('uno');
+    break;
+    case 2: document.write('dos');
+    break;
+    case 3: document.write('tres');
+    break;
+    case 4: document.write('cuatro');
+    break;
+    case 5: document.write('cinco');
+    break;
+    default: document.write('Debe ingresar un valor comprendido entre 1 y 5.');
+}
